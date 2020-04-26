98e0600be033a3e1af6cab350acf4f05d90027d5
Tu Nombre <nesnae@gmail.com>
2020-04-26 16:31:50 -0400

    division con resto

--- division_cociente.py
+++ division_cociente.py
@@ -5,5 +5,18 @@ Created on Sun Apr 26 14:39:41 2020
 
 @author: jeanwolf
 """
+# programa para hacer divisiones
 
+print ("Hola Isabel")
+
+divisor = (int(input("Introduce el divisor: ")))
+dividendo = (int(input("Introduce el dividendo: ")))
+print ("el divisor es: ", divisor)
+print ("el dividendo es: ", dividendo)
+
+# aqui muestra el cociente y resto
+print("********")
+print("El resultado es: ", divmod(divisor, dividendo))
+print("********")
+print("no olvides que el primer numero es el Resultado y el segundo el Resto")
 
